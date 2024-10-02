00090e537e2407b84129f316d44460be44f92004
Marc Jerick <umarcjerick@gmail.com>
2024-10-02 22:30:51 +0800

    Completed Variables section

--- passwordPrompt.js
+++ passwordPrompt.js
@@ -1,14 +1,18 @@
+// Importing the prompt-sync
 const ps = require("prompt-sync");
 const prompt = ps();
-
+//the correct pasword and the user password
 let correctPassword = "Angcuteko";
 let userPassword;
-
+//do while loop
 do {
+    //asking the user to enter the password
     userPassword = prompt("Enter your password: ");
+    //checking if the password is incorrect
     if (userPassword !== correctPassword) {
         console.log("Incorrect, try again");
     }
+    //checking the password if correct
 } while (userPassword !== correctPassword);
 
 console.log("Access granted");
